f2394b80f7a636ab1f31092b8f4c46f0f8a5942f
Administrator <1104538944@qq.com>
2023-03-07 01:13:00 +0800

    1

--- pages/login/camouflage.vue
+++ pages/login/camouflage.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="main">
-		<block v-if="show">
+		<block v-if="!show">
 			<text class="title">活动参与资格报名</text>
 			<view class="form">
 				<view class="form-item flex flex-align-end">
@@ -27,27 +27,51 @@
 			</view>
 		</block>
 		<block v-else>
-			<view class="qrcode">
+			<view class="qrcode" @click="tabBarHome">
 				<image :src="staticUrl + 'logo_qrcode.jpeg'" class="icon"></image>
-				<text class="font36 color-b6">进入RedMall小程序</text>
+				<text class="font36 color-b6" style="background: #3888fff2;padding: 20rpx;color: white;font-weight: bolder;border-radius: 30rpx;margin-top: 50rpx;text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px #2626ef;">
+					进入RedMall小程序
+				</text>
 			</view>
 		</block>
 	</view>
 </template>
 
 <script>
+	import url from "@/common/http/url.js"
+	
 	export default{
 		data() {
 			return {
-				staticUrl: getApp().staticUrl,
+				//staticUrl: getApp().staticUrl,
+				staticUrl: this.$staticUrl,
 				form: {},
-				show: false
+				show: false,
+				uniPlatform: uni.getSystemInfoSync().uniPlatform,
+			}
+		},
+		onShow() {
+			// 获取 苹果应用商店需要隐藏的页面和功能模块 配置
+			if (this.uniPlatform === 'mp-weixin') {
+				this.$http("GET", url.common.mpWeiXiHide).then(res => {
+					if(res.data){
+						this.show = res.data.keyValue === "1" ? false : true
+					}
+				})
+			}else{//其它设备则显示
+				this.show = true
 			}
 		},
 		methods: {
 			onSumbit() {
 				this.$msg("报名成功！当前限时免费，无需报名费!")
 				// this.loading = true
+			},
+			tabBarHome(){
+				//跳转到 tabBar 页面，并关闭其他所有非 tabBar 页面。
+				uni.reLaunch({
+					url: '/pages/home/index'
+				});
 			}
 		}
 	}
